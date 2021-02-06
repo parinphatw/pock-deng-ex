f972c05a71b56572b82607536316f5dcd95976ca
Parinphat Wasinchaikulpat <parinphatw@gmail.com>
2021-02-06 17:48:27 +0700

    add more test case to manage.js

--- test/manage.test.js
+++ test/manage.test.js
@@ -1,8 +1,16 @@
-const { test } = require("@jest/globals");
+const { test, expect } = require("@jest/globals");
 const manage = require("../utils/manage");
 
 test("calculate winner in case the player wins", () => {
   expect(
     manage.calculateWinner(["Diamonds-A", "Hearts-6"], ["Clubs-5", "Hearts-Q"])
   ).toBe("player");
+});
+
+test("calculate balance of the player in case the player wins", () => {
+  expect(manage.calculateBalance("player", 60, 30)).toBe(90);
+});
+
+test("calculate balance of the player in case the player loses", () => {
+  expect(manage.calculateBalance("dealer", 60, 30)).toBe(30);
 });
