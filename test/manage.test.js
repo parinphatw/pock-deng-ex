d1a9bf72b0f38605ce1cb3d1a5fa9df3ac0b8200
Parinphat Wasinchaikulpat <parinphatw@gmail.com>
2021-02-06 17:55:32 +0700

    add all the cases feasible for manage.js

--- test/manage.test.js
+++ test/manage.test.js
@@ -7,10 +7,26 @@ test("calculate winner in case the player wins", () => {
   ).toBe("player");
 });
 
+test("calculate winner in case the player loses", () => {
+  expect(
+    manage.calculateWinner(["Diamonds-A", "Hearts-2"], ["Clubs-5", "Hearts-Q"])
+  ).toBe("dealer");
+});
+
+test("calculate winner in case the player ties", () => {
+  expect(
+    manage.calculateWinner(["Diamonds-A", "Hearts-6"], ["Clubs-5", "Hearts-2"])
+  ).toBe("tie");
+});
+
 test("calculate balance of the player in case the player wins", () => {
   expect(manage.calculateBalance("player", 60, 30)).toBe(90);
 });
 
 test("calculate balance of the player in case the player loses", () => {
   expect(manage.calculateBalance("dealer", 60, 30)).toBe(30);
+});
+
+test("calculate balance of the player in case the player ties", () => {
+  expect(manage.calculateBalance("tie", 60, 30)).toBe(60);
 });
